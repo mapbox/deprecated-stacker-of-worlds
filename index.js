f1aad7d56949b7af7ebee619c526bf21dd9e1a15
David Clark <dclark@mapbox.com>
2016-08-13 08:41:18 -0400

    Finally something presentable

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 'use strict';
 
 const turf = require('turf');
-const superSphere = require('./lib/super-sphere');
+const projector = require('./lib/projector');
 
-// The extra degree of longitude is to produce some overlap, so there is
-// not a strict cut-off line at the antimeridian
 const realWorldOutline = [
-  [-181, 90],
-  [181, 90],
-  [181, -90],
-  [-181, -90],
-  [-181, 90],
+  [-180, 90],
+  [180, 90],
+  [180, -90],
+  [-180, -90],
+  [-180, 90],
 ];
 
 const worldCache = {};
-function createWorld(worldNumber) {
+function createProjectedWorld(worldNumber) {
   if (worldCache[worldNumber]) return worldCache[worldNumber];
 
   const worldOutline = realWorldOutline
-    .map(function(lngLat) {
-      return superSphere.lngLat2xy([lngLat[0] + 360 * worldNumber, lngLat[1]]);
+    .map((lngLat) => {
+      return projector.lngLat2xy([lngLat[0] + 360 * worldNumber, lngLat[1]]);
     });
 
   const world = {
@@ -38,49 +36,63 @@ function createWorld(worldNumber) {
 function stackMultiWorldFeature(feature) {
   if (feature.type === 'FeatureCollection') {
     return Object.assign({}, feature, {
-      features: feature.features.reduce(function(result, subFeature) {
-        return result.concat(stackMultiWorldFeature(subFeature));
-      }, []),
+      features: feature.features.map(stackMultiWorldFeature),
     });
   }
 
   const bbox = turf.bbox(feature);
   const rangeOfWorlds = [Math.floor(bbox[0] / 360), Math.ceil(bbox[2] / 360)];
-  const unprojectedFeature = transformFeatureCoordinates(feature, superSphere.lngLat2xy);
+  const projectedFeatureCoordinates = transformCoordinates(feature.geometry.coordinates, projector.lngLat2xy);
+  const projectedFeature = swapCoordinates(feature, projectedFeatureCoordinates);
 
-  const stackedFeatures = [];
+  const stackedCoordinates = [];
   for (let worldNumber = rangeOfWorlds[0]; worldNumber <= rangeOfWorlds[1]; worldNumber++) {
-    const world = createWorld(worldNumber);
-    const intersection = turf.intersect(unprojectedFeature, world);
+    const projectedWorld = createProjectedWorld(worldNumber);
+    const intersection = turf.intersect(projectedFeature, projectedWorld);
     if (!intersection) continue;
 
     const lngOffset = worldNumber * -360;
-    const newFeature = transformFeatureCoordinates(intersection, function(lngLat) {
-      const projectedLngLat = superSphere.xy2LngLat(lngLat);
+    const unprojectedIntersectionCoordinates = transformCoordinates(intersection.geometry.coordinates, (lngLat) => {
+      const projectedLngLat = projector.xy2LngLat(lngLat);
       return [projectedLngLat[0] + lngOffset, projectedLngLat[1]];
     });
-    newFeature.properties = feature.properties;
 
-    // id will not be unique anymore if the feature is split
-    delete newFeature.properties.id;
-
-    stackedFeatures.push(newFeature);
+    if (intersection.geometry.type.indexOf('Multi') === 0) {
+      unprojectedIntersectionCoordinates.forEach((coordinates) => {
+        stackedCoordinates.push(coordinates);
+      });
+    } else {
+      stackedCoordinates.push(unprojectedIntersectionCoordinates);
+    }
   }
 
-  return stackedFeatures;
+  if (stackedCoordinates.length === 1) {
+    return swapCoordinates(feature, stackedCoordinates[0]);
+  }
+
+  const multiType = (feature.geometry.type.indexOf('Multi') === 0)
+    ? feature.geometry.type
+    : `Multi${feature.geometry.type}`;
+
+  return Object.assign({}, feature, {
+    geometry: {
+      type: multiType,
+      coordinates: stackedCoordinates,
+    },
+  });
 }
 
 function transformCoordinates(coordinates, transform) {
   if (Array.isArray(coordinates[0])) {
-    return coordinates.map(subValue => transformCoordinates(subValue.slice(), transform));
+    return coordinates.map((subValue) => transformCoordinates(subValue.slice(), transform));
   }
   return transform(coordinates.slice());
 }
 
-function transformFeatureCoordinates(feature, transform) {
+function swapCoordinates(feature, newCoordinates) {
   return Object.assign({}, feature, {
     geometry: Object.assign({}, feature.geometry, {
-      coordinates: transformCoordinates(feature.geometry.coordinates, transform),
+      coordinates: newCoordinates,
     }),
   });
 }
